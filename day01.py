f54887248195b64b8e4638a033e303383b9fd4ce
王尹滋 <1586860604@qq.com>
2026-08-27 15:48:58 +0800

    ai教学day01

--- day01.py
+++ day01.py
@@ -1,1 +1,7 @@
 print("Hello,我要转行做Python开发了!")
+name = "卢本伟"
+age = 27
+city = "西安"
+target_salary = 8000
+
+print("我叫" + name + ", 今年" +str(age) + "岁, 在" + city + ", 我的目标薪资是" + str(target_salary) + "元/月")
