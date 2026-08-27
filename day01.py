c531518eceec8837551032c960725214635bc0a5
王尹滋 <1586860604@qq.com>
2026-08-27 16:10:44 +0800

    day01完成：input/f-string/注释，基础输出练习

--- day01.py
+++ day01.py
@@ -1,7 +1,18 @@
+# Day 01: 打印输出、变量、字符串拼接
+# 目标：用变量保存个人信息并输出
 print("Hello,我要转行做Python开发了!")
-name = "卢本伟"
-age = 27
-city = "西安"
-target_salary = 8000
+name = input("请输入你的名字：")
+age = int(input("请输入你的年龄："))
+city = input("请输入你所在的城市：")
+target_salary = int(input("请输入你的目标薪资："))
 
-print("我叫" + name + ", 今年" +str(age) + "岁, 在" + city + ", 我的目标薪资是" + str(target_salary) + "元/月")
+# 字符串拼接写法
+# print("我叫" + name + ", 今年" +str(age) + "岁, 在" + city + ", 我的目标薪资是" + str(target_salary) + "元/月")
+
+# f-string写法
+print(f"我叫{name}, 今年{age}岁, 在{city}, 我的目标薪资是{target_salary}元/月")
+print("--- 个人信息 ---")
+print(f"姓名：{name}")
+print(f"年龄：{age}")
+print(f"城市：{city}")
+print(f"目标薪资：{target_salary} 元/月")
