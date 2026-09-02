0cd86517ed87dd5b1f6462a07c28270f6d1dc90d
王尹滋 <1586860604@qq.com>
2026-09-02 18:00:38 +0800

    day06字典和集合

---
+++ day06.py
@@ -0,0 +1,83 @@
+# 字典(dict) + 集合(set)的使用
+person = {
+    "name": "卢本伟",
+    "age": 27,
+    "city": "西安"
+}
+
+print(person["name"])  # 卢本伟
+print(person["age"])   # 27
+print(person["city"])  # 西安
+
+person = {"name": "卢本伟", "age": 27}
+
+# 改
+person["age"] = 28
+print(person) #{"name": "卢本伟", "age": 28}
+
+# 增(键不存在就自动新增)
+person["salary"] = 8000
+print(person)  #{"name": "卢本伟", "age": 28, "salary": 8000}
+
+# 查(get,键不存在不会报错)
+print(person.get("name"))  # 卢本伟
+print(person.get("gender"))  # None
+print(person.get("height",168))  # 168，键不存在时返回默认值
+
+#  删
+person.pop("salary")  # 删除键为"salary"的键值对
+print(person)  #{"name": "卢本伟", "age": 28}
+
+# 删全部
+# person.clear()
+
+# 字典的三种视图
+person = {"name": "卢本伟", "age": 28, "city": "西安"}
+
+print(person.keys())    # dict_keys(['name', 'age', 'city']) 所有键
+print(person.values())  # dict_values(['卢本伟', 28, '西安']) 所有值
+print(person.items())   # dict_items([('name', '卢本伟'), ('age', 28), ('city', '西安')]) 所有键值对
+
+# 遍历字典
+for key, value in person.items():
+    print(f"{key}: {value}")
+
+person = {"name": "卢本伟", "age": 27, "city": "西安"}
+
+# 只遍历键(默认就是键)
+for key in person:
+    print(key)
+
+# 只遍历值
+for value in person.values():
+    print(value)
+
+# 同时遍历键和值
+for key, value in person.items():
+    print(key, value)
+
+# 集合(set)的使用
+# 集合是无序的、不可重复的数据集合
+nums = {1, 2, 2, 3, 3, 4}
+# 如果加一行unique = set(nums)，那么unique就是一个新的集合，里面的元素是nums去重后的结果，相当于把去重后的结果保留下来了
+print(nums)  # {1, 2, 3, 4}，重复的元素会被自动去重
+
+# 空集合不能写成 {}，因为 {} 是空字典的语法，空集合需要使用 set() 来创建
+empty_set = set()
+
+# 集合常用操作
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+print(a & b)  # {3, 4}，交集
+print(a | b)  # {1, 2, 3, 4, 5, 6}，并集
+print(a - b)  # {1, 2}，差集
+print(b - a)  # {5, 6}，差集
+print(a ^ b)  # {1, 2, 5, 6}，对称差集
+
+# 判断在不在
+nums = {1, 2, 3}
+print(2 in nums)  # True
+print(4 in nums)  # False
+# set判断in的时间复杂度是O(1)，而list判断in的时间复杂度是O(n)，所以set在判断元素是否存在时效率更高。
+
